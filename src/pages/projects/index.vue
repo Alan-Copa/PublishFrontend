3afeaa1a8afc10db229191a4bcd9ed1e2943d51d
Alan Copa <alan.copa2001@gmail.com>
2025-02-12 00:38:56 +0100

    more projects

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -47,7 +47,7 @@ const slides = ref([
   },
   { 
     title: "Smart Estate", 
-    content: "A blockchain-based real estate platform", 
+    content: "A blockchain-based real estate platform. The DApp leverages blockchain and smart contracts to enable fractional ownership of real estate through NFT tokenization. ", 
     imgname: "smart-estate.png", 
     year: 2024, 
     type: "University",
@@ -58,7 +58,7 @@ const slides = ref([
   },
   { 
     title: "Smart Contracts: Crowdfunding", 
-    content: "Ethereum-based smart contracts", 
+    content: "Ethereum-based smart contracts for crowdfunding", 
     imgname: "crwdf.png", 
     year: 2024, 
     type: "Personal",
@@ -69,7 +69,7 @@ const slides = ref([
   },
   { 
     title: "Data Visualization Portfolio", 
-    content: "Interactive Data Visualization Portfolio", 
+    content: "Interactive Data Visualization Portfolio. The project explores different visualization techniques, critiques common design flaws, and applies best practices for effective data communication.", 
     imgname: "dataviz.png", 
     year: 2024, 
     type: "University",
@@ -80,7 +80,7 @@ const slides = ref([
   },
   {
     title: "slite.ai", 
-    content: "pitch deck for a startup about autonomous rovers delivering food", 
+    content: "Slite.ai is a revolutionary startup focused on transforming urban food and grocery delivery through autonomous rovers. Download the pitch deck below", 
     imgname: "slite-ai.png", 
     year: 2024, 
     type: "University",
@@ -103,7 +103,7 @@ const slides = ref([
   },
   { 
     title: "Customer Relationship Management", 
-    content: "A CRM system I developed for a company", 
+    content: "Web application designed to optimize internal operations, improve customer management, and streamline communication within the company. Developed as a centralized platform, it enables efficient tracking of client activities, internal collaboration, and secure data storage.", 
     imgname: "CRM.png", 
     year: 2023, 
     type: "Company",
@@ -113,8 +113,20 @@ const slides = ref([
     DemoLink: "/book-demo"
   },
   { 
-    title: "Web Crawler", 
-    content: "A Python web scraper that collects and indexes house rental listings, with search functionality via an Express.js frontend.", 
+    title: "VRtual Arena", 
+    content: "VRtual Arena is an innovative virtual reality (VR) solution that reimagines the sports entertainment industry. It offers an immersive experience that puts fans right in the action.", 
+    imgname: "VRtual-arena.png", 
+    year: 2023, 
+    type: "University",
+    GdriveLink: "",
+    GithubLink: "",
+    externalLink: "",
+    PDFpath: "/projects/VRtual_Arena.pdf",
+    DemoLink: "/book-demo", 
+  },
+  { 
+    title: "Retrieval System", 
+    content: "A Python web scraper that collects and indexes house rental listings with Solr, with search functionality via an Express.js frontend.", 
     imgname: "webCrawler.png", 
     year: 2023, 
     type: "University",
@@ -125,7 +137,7 @@ const slides = ref([
   },
   { 
     title: "Telegram Bot",
-    content: "Telegram bot to regiter users",
+    content: "Telegram bot to register users",
     imgname: "telegram.png",
     year: 2023,
     type: "Company",
@@ -175,6 +187,17 @@ const slides = ref([
     type: "Personal",
     GdriveLink: "",
     GithubLink: "https://github.com/Alan-Copa/Instagram_Bot",
+    externalLink: "",
+    DemoLink: "/book-demo"
+  },
+  {
+    title: "Philosophy",
+    content: "just.. Philosophy",
+    imgname: "philosophy.png",
+    year: 2023,
+    type: "Personal",
+    GdriveLink: "",
+    GithubLink: "  https://github.com/Alan-Copa/antropologic-universalism",
     externalLink: "",
     DemoLink: "/book-demo"
   },
